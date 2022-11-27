8b0b8a730e20434462f58cc462fa01c07271190e
catdroutcode <xcatdrout@proton.me>
2022-11-27 18:10:01 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import * as ackeeTracker from 'ackee-tracker'
+
+ackeeTracker.create('https://ackee.catdrout.xyz').record('1e62c274-6f41-4a77-b536-00557518e1a9')
 
 export default function Document() {
   return (
