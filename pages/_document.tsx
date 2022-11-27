582ed7e14268cf21bddaa8bf14af284160ef3d2c
catdroutcode <xcatdrout@proton.me>
2022-11-27 18:19:53 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import * as ackeeTracker from 'ackee-tracker'
-const location = "https://catdrout.xyz/"
 
-ackeeTracker.create('https://ackee.catdrout.xyz').record('1e62c274-6f41-4a77-b536-00557518e1a9')
+ackeeTracker.detect()
 
 export default function Document() {
   return (
-    <Html className="dark">
+    <Html className="dark" hidden data-ackee-server="https://ackee.catdrout.xyz" data-ackee-domain-id="1e62c274-6f41-4a77-b536-00557518e1a9">
       <Head>
       </Head>
       <body>
